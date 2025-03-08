9954c8271fdcab0f23323e462d868483b2a4b98a
Tox1GeN <kovalevich.danil@gmail.com>
2025-03-09 00:17:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There will be the game (pretty soon)");
+        }
+    }
+}
