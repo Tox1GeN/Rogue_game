5ab68b89597df30dc27690d16764a52a4f26973f
Tox1GeN <kovalevich.danil@gmail.com>
2025-03-09 20:10:15 +0100

    Field Initial Logic

---
+++ Core/Cell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogue.Core
+{
+    internal class Cell
+    {
+        // Each cell can be a wall or empty cell
+        public bool IsWall { get; set; } = false;
+
+        // Empty cell can have one item or even more in it
+        // List of items in the cell
+        public Stack<Rogue.Models.Item> Items { get; set; } = new Stack<Rogue.Models.Item>();
+
+        public bool IsPlayerHere { get; set; } = false;
+
+        // Get character of the current cell for a rendering
+        public char GetDisplayCell()
+        {
+            if (IsPlayerHere)
+                return '¶';
+            if (IsWall)
+                return '█';
+            if (Items.Count > 0)
+                return '*';
+
+            // Otherwise it is an empty cell
+            return ' ';
+        }
+
+        // Consturctor ?
+        //public Cell(){}
+
+    }
+}
---
+++ Core/Room.cs
@@ -0,0 +1,63 @@
+﻿using Rogue.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogue.Core
+{
+    internal class Room
+    {
+        // Game field
+        public Cell[,] Grid { get; } = new Cell[20, 40];
+
+        // Player's position
+        public (int row_Y, int col_X) PlayerPosition { get; set; }
+
+        // Initialize the room
+        public Room()
+        {
+            // each cell in the grid
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    Grid[i, j] = new Cell();
+                }
+            }
+
+            // Spawn the Player
+            Grid[0, 0].IsPlayerHere = true;
+            PlayerPosition = (0, 0);
+        }
+
+        // Render the room 
+        // TODO: change the render logic line by line, not char by char to make it faster and smother
+        public void Render()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    Console.Write(Grid[i, j].GetDisplayCell());
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public Item? RemoveTopItemAt(int row_Y, int col_X)
+        {
+            if (row_Y < 0 || row_Y > 19 || col_X < 0 || col_X > 39 || Grid[row_Y, col_X].IsWall)
+                return null;
+            return Grid[row_Y, col_X].Items.Count > 0 ? Grid[row_Y, col_X].Items.Pop() : null;
+        }
+        public void ReceiveDropItem(int row_Y, int col_X, Item dropItem)
+        {
+            if (row_Y < 0 || row_Y > 19 || col_X < 0 || col_X > 39 || Grid[row_Y, col_X].IsWall)
+                return;
+            Grid[row_Y, col_X].Items.Push(dropItem);
+        }
+    }
+}
