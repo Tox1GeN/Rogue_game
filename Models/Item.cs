1588f2b9eb9a91ecbeb413804ec1034bfbf1c693
Tox1GeN <kovalevich.danil@gmail.com>
2025-03-10 01:50:24 +0100

    Item modification and Item's Decorator

--- Models/Item.cs
+++ Models/Item.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace Rogue.Models
 {
+    internal enum HandRequirement
+    {
+        None,  // For items that cannot be equipped
+        One,   // For one-handed items
+        Two    // For two-handed items
+    }
     internal class Item
     {
         public virtual string Name { get; protected set; }
         public virtual string GetDisplayName() => Name;
         public virtual void Equip(Player player) { }
         public virtual void Unequip(Player player) { }
+
+        // By default the item is unequipable. Override it in a child class
+        public virtual HandRequirement Requirement => HandRequirement.None;
     }
 }
