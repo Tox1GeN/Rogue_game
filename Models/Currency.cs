952f362139664f9097f7420655211fec2112eaca
Tox1GeN <kovalevich.danil@gmail.com>
2025-03-10 01:52:02 +0100

    New Items: Weapons, Currency & Unusable

---
+++ Models/Currency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogue.Models
+{
+    internal enum CurrencyType { Coin, Gold }
+    internal class Currency : Item
+    {
+        // Private set for value and the type to liquidate the cheating:
+        // - money duplication;
+        // - changing the currency to multiply the value of money
+        public int Value { get; private set; }
+        public CurrencyType Type { get; private set; }
+
+        public Currency(CurrencyType type, int value)
+        {
+            Type = type;
+            Value = value;
+            Name = type.ToString();
+        }
+
+        public override string GetDisplayName() => $"{Name} ({Value})";
+
+        // NO need to override Equip and Unequip methods
+    }
+}
---
+++ Models/UnusableItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogue.Models
+{
+    internal class UnusableItem: Item
+    {
+        public string Descripton { get; set; }
+        public UnusableItem(string name, string desc)
+        {
+            Name = name;
+            Descripton = desc;
+        }
+
+        public override string GetDisplayName() => Name;
+        public string GetDisplayDescription() => Descripton;
+    }
+}
