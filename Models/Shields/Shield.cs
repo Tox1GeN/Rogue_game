86dcc44024675465ebf8d94cc80cb80cceb513d0
Tox1GeN <kovalevich.danil@gmail.com>
2025-03-11 02:34:05 +0100

    Shields Idea

---
+++ Models/Shields/Shield.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogue.Models.Shields
+{
+    internal class ShieldItem : Equipment
+    {
+        public int ArmorBonus { get; private set; }
+        public int ParryDamage { get; private set; }
+        public int MiliSecondsToParry { get; private set; }
+        public override HandRequirement Requirement => HandRequirement.One;
+
+        // The future idea to add short commet on equip action. For Example:
+        // You've equipped Excalibur. You fill Blessed
+        // public string ShortReview { get; set; }
+
+        // One more similar idea, but for unequip.
+        // You've unequipped the Curse Sword. You've not felt better.
+        // public string SecretMessage { get; set; }
+
+        public ShieldItem(int armor, int parryDmg, int timeParry)
+        {
+            ArmorBonus = armor;
+            ParryDamage = parryDmg;
+            MiliSecondsToParry = timeParry;
+        }
+    }
+}
